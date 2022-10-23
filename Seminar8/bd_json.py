fba3aa148a1327f283389e7036e6150da4b5be9e
julemalysheva <julemalysheva@gmail.com>
2022-10-23 10:32:20 +0500

    Форма входа,загрузка/выгрузка

--- Seminar8/bd_json.py
+++ Seminar8/bd_json.py
@@ -1,16 +1,19 @@
 import json
 from main import staff #сама переменная инициализируется и формируется в модуле main 
 import logger as log
+from easygui import *
 
 def load():
     global staff
     with open("staff.json", "r", encoding="utf-8") as fl:
         staff = json.load(fl)
         log.register_logger("База сотрудников загружена")
-    print("База сотрудников загружена")
+    msgbox("База сотрудников загружена")
+    # print("База сотрудников загружена")
 
-def save():
+def save(staff):
     with open("staff.json", "w", encoding="utf-8") as fl:
         fl.write(json.dumps(staff, ensure_ascii=False))
         log.register_logger("База успешно сохранена в файле staff.json")
-    print('База сотрудников была успешно сохранена в файле staff.json')
+    msgbox('База сотрудников была успешно сохранена в файле staff.json')    
+    # print('База сотрудников была успешно сохранена в файле staff.json')
--- Seminar8/logger.py
+++ Seminar8/logger.py
@@ -11,21 +11,21 @@ from datetime import datetime as dt
 from time import time
 # записываем в лог вход и выход пользователя
 def register_logger(data=''):
-    with open('log.txt', 'a',encoding="utf8") as file:
+    with open('log_staff.txt', 'a',encoding="utf8") as file:
         file.write('{} Зафиксировано событие {}\n'
                     .format(dt.now().strftime('%d.%m.%Y-%H:%M'), data))
 # записываем в лог вариант меню с запросом пользователя
 def menu_logger(data=''):
-    with open('log.txt', 'a',encoding="utf8") as file:
+    with open('log_staff.txt', 'a',encoding="utf8") as file:
         file.write('{} Выбран пункт меню {}\n'
                     .format(dt.now().strftime('%d.%m.%Y-%H:%M'), data))
 # записываем в лог (фиксацию) ошибки с указанием даты/времени 
 def error_logger(data=''):
-    with open('log.txt', 'a',encoding="utf8") as file:
+    with open('log_staff.txt', 'a',encoding="utf8") as file:
         file.write('{} Зафиксирована ошибка {}\n' #по хорошему как-то обозначить, какая
                     .format(dt.now().strftime('%d.%m.%Y-%H:%M'), data))
 # записываем в лог выдачу результата запроса
 def res_logger(data=''):
-    with open('log.txt', 'a',encoding="utf8") as file:
+    with open('log_staff.txt', 'a',encoding="utf8") as file:
         file.write('{} Получен результат запроса {}\n'
                     .format(dt.now().strftime('%d.%m.%Y-%H:%M'), data))
