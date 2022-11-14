d5e83eeaa41e2aae6a58612092f2fbff1a572d44
julemalysheva <julemalysheva@gmail.com>
2022-11-14 19:57:58 +0500

    bootcamp  Html, Css, Jinja

--- flaskbootcamp/main.py
+++ flaskbootcamp/main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
-from random import randint as rd
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    name = rd(0, 1)
-    return render_template('base.html', name=name)
+    with open('flaskbootcamp/file.txt', 'r', encoding='utf-8') as file:
+        resultData = list()
+        for line in file.readlines():
+            resultData.append(tuple(line.split('\n')[0].split(';')))
+
+    return render_template('base.html', data=resultData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
